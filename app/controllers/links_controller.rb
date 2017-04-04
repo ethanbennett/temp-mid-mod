52ee07b054df434241e12b24ff182ba8968f02c4
Ethan Bennett <ebennett19@gmail.com>
2017-04-04 17:50:57 -0600

    Refactor link functionality

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,9 +5,9 @@ class LinksController < ApplicationController
   def index
     if !current_user
       redirect_to login_path
+    else
+      @link = @user.links.new
     end
-    @link = @user.links.new
-
   end
 
   def create
