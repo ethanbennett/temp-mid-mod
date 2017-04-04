031c62f72c13346f8d435b8a2817c79b4aaa6e48
Ethan Bennett <ebennett19@gmail.com>
2017-04-04 11:50:03 -0600

    Implement flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    binding.pry
-    if @user.save
+    if !valid_email
+      flash[:invalid_email] = "Please enter a valid email address"
+      render 'new'
+    elsif password_mismatch
+      flash[:password_mismatch] = "Please make sure your password confirmation matches your password"
+      render 'new'
+    elsif !valid_password
+      flash[:invalid_password] = "Please enter a valid password"
+      render 'new'
+    else @user.save
       session[:user_id] = @user.id
       redirect_to root_path, notice: "Welcome to the best app you'll ever belong to"
-    else
-      flash[:error] = 'An error occured!'
-      render 'new'
     end
   end
 
@@ -19,5 +24,17 @@ private
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def valid_email
+    user_params["email"].include? "@"
+  end
+
+  def password_mismatch
+    user_params["password"] != user_params["password_confirmation"]
+  end
+
+  def valid_password
+    user_params["password"] != ""
   end
 end
