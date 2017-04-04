f08afc31e8e003c5c489c4d701fc7ed3ef4acbc9
Ethan Bennett <ebennett19@gmail.com>
2017-04-04 11:29:18 -0600

    Implement basic create account functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    binding.pry
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Welcome to the best app you'll ever belong to"
+    else
+      flash[:error] = 'An error occured!'
+      render 'new'
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
