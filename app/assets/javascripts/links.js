e65c3a31b5ad635440b805f962c46eb410f96030
Ethan Bennett <ebennett19@gmail.com>
2017-04-04 20:13:17 -0600

    Done for the night

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -1,32 +1,32 @@
-// $(document).ready(function(){
-//   bindSubmitListenerAndPostLink();
+$(document).ready(function(){
+  bindSubmitListenerAndPostLink();
 
-// function bindSubmitListenerAndPostLink() {
-//   $("#new_link").submit(function(event) {
-//       // event.preventDefault();
-//       postLink();
-//   });
-// }
+function bindSubmitListenerAndPostLink() {
+  $("#new_link").submit(function(event) {
+      event.preventDefault();
+      postLink();
+  });
+}
 
-// function postLink() {
-//   var linkData = {
-//     link: {
-//       title: $("#link_title").val(),
-//       url: $("#link_url").val(),
-//     }
-//   }
-//   console.log(linkData);
+function postLink() {
+  var linkData = {
+    link: {
+      title: $("#link_title").val(),
+      url: $("#link_url").val(),
+    }
+  }
+  console.log(linkData);
 
-//     $.ajax({
-//               url: "/api/v1/links",
-//               method: "POST",
-//               data: linkData
-//               })
-//               .done(function(newLinkMarkup) {
-//                   $(".link-list").append(newLinkMarkup);
-//                   $("#link_title").val("");
-//                   $("#link_url").val("");
-//               });
-//   }
+    $.ajax({
+              url: "/api/v1/links",
+              method: "POST",
+              data: linkData
+              })
+              .done(function(newLinkMarkup) {
+                  $(".link-list").append(newLinkMarkup);
+                  $("#link_title").val("");
+                  $("#link_url").val("");
+              });
+  }
 
-// });
+});
