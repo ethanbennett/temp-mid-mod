01d3a9ae6132149ba8bab668d38d602ad6fde5e4
Ethan Bennett <ebennett19@gmail.com>
2017-04-17 11:52:34 -0600

    Add validations to link model. Keeps logic in error helper for better error handling

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ActiveRecord::Base
   belongs_to :user
   validates :url, :title, presence: true
+  validates_format_of :url, :with => URI::regexp(%w(http https))
+  validates_uniqueness_of :title, scope: :user_id
+  validates_uniqueness_of :url, scope: :user_id
 end
