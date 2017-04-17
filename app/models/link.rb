e7f3bc252f84c11b614343d3b181730917f7a138
Ethan Bennett <ebennett19@gmail.com>
2017-04-17 12:00:04 -0600

    Fix broken submission functionality

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,5 +3,4 @@ class Link < ActiveRecord::Base
   validates :url, :title, presence: true
   validates_format_of :url, :with => URI::regexp(%w(http https))
   validates_uniqueness_of :title, scope: :user_id
-  validates_uniqueness_of :url, scope: :user_id
 end
