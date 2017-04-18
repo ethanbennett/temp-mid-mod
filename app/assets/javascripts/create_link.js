2379df5c53d1fda02339a0914e5d8ef8df0e4f52
Ethan Bennett <ebennett19@gmail.com>
2017-04-18 01:44:10 -0600

    readd js

--- app/assets/javascripts/create_link.js
+++ app/assets/javascripts/create_link.js
@@ -1,44 +1,44 @@
-// $( document ).ready(function(){
-//   $("#create-link").on("click", createLink)
-// });
+$( document ).ready(function(){
+  $("#create-link").on("click", createLink)
+});
 
-// function createLink(e) {
-//   e.preventDefault();
-//   var link = formatLink();
-//   addLink(link);
-// }
+function createLink(e) {
+  e.preventDefault();
+  var link = formatLink();
+  addLink(link);
+}
 
-// function formatLink () {
-//   return {
-//     link: {
-//       url: $("#url").val(),
-//       title: $("#title").val()
-//     }
-//   }
-// }
+function formatLink () {
+  return {
+    link: {
+      url: $("#url").val(),
+      title: $("#title").val()
+    }
+  }
+}
 
-// function addLink(link) {
-//   var newHtml = formatLinkHtml()
+function addLink(link) {
+  var newHtml = formatLinkHtml()
 
-//   $.ajax({
-//     type: "POST",
-//     url: "/api/v1/links",
-//     data: link
-//   }).done( (response) => {
-//     $("#links-list").prepend(newHtml)
-//   }).fail(error => console.log(error))
-// }
+  $.ajax({
+    type: "POST",
+    url: "/api/v1/links",
+    data: link
+  }).done( (response) => {
+    $("#links-list").prepend(newHtml)
+  }).fail(error => console.log(error))
+}
 
-//   function formatLinkHtml() {
-//     var title = $("#title").val()
-//     var url = $("#url").val()
-//     var linkId = $("#link-id").val()
+  function formatLinkHtml() {
+    var title = $("#title").val()
+    var url = $("#url").val()
+    var linkId = $("#link-id").val()
 
-//     return "<div class='read-false'><strong>" +
-//     title + "</strong></br><a href='" +
-//     url + "'>" +
-//     url + "</a></br><a href='/links/" +
-//     linkId +"/edit'>Edit</a></br><button class='mark-read'>Mark Read</button></br></br>"
-// }
+    return "<div class='read-false'><strong>" +
+    title + "</strong></br><a href='" +
+    url + "'>" +
+    url + "</a></br><a href='/links/" +
+    linkId +"/edit'>Edit</a></br><button class='mark-read'>Mark Read</button></br></br>"
+}
 
 
