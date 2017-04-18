9aee2e65fb7067e1015f1b69c4236e357617457a
Ethan Bennett <ebennett19@gmail.com>
2017-04-18 14:36:41 -0600

    Fix create method

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::LinksController < ApplicationController
   end
 
   def create
-    @link = current_user.links.new(url: params["link"]["url"], title: params["link"]["title"])
+    @link = current_user.links.new(url: params["link"]["url"], title: params["link"]["title"], count: 0)
     if @link.save
       render partial: "link", locals: {link: @link}
     else
